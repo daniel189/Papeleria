e8d63884db7cadb38594edfda0f154fe501c75a1
Daniel Ortega <danielhop189@hotmail.com>
2017-11-21 20:03:22 -0500

    Areglo de la clase iva

--- papeleria-farfarela/src/papeleriafarfarela/FechaIva.java
+++ papeleria-farfarela/src/papeleriafarfarela/FechaIva.java
@@ -11,7 +11,7 @@ import java.util.*;
 /** Asociación entre tabla IVA y ARTICULOS
  * 
  * @pdOid e2feec9c-7238-4633-b404-0d4d04d5f31b */
-public class Fechaiva {
+public class FechaIva {
    /** @pdOid 90af23e7-9e0e-4c94-97e8-6e3cc6fdd289 */
    public java.util.Date fecInicio;
    /** @pdOid bca6b240-cd12-429f-809a-3ba60bdfe36e */
